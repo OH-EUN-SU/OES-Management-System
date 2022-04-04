40d6f785352ea851d616cfb3a82c77092186f37d
eun-su-oh <0221dmstn@naver.com>
2022-04-04 12:09:06 +0900

    changed

--- src/classProject/Lesson.java
+++ src/classProject/Lesson.java
@@ -16,7 +16,7 @@ public class Lesson {
 		public void printInfo() {
 			for(int i = 0; i < count; i++) {
 				System.out.println("이름: " + name.get(i));               //저장된 이름을 출력
-				System.out.println("전화번호: " + phone_number.get(i));    //저장된 전화버노를 출력
+				System.out.println("전화번호: " + phone_number.get(i));    //저장된 전화번호를 출력
 				System.out.println("과목: " + subject.get(i));            //저장된 과목을 출력
 				System.out.println("목표 등급: "+ goal.get(i));            //저장된 등급을 출력
 				System.out.println();
